824bcd51755b092528ca5caac39d3e9dec13306c
npikall <nikolas.pikall@gmail.com>
2025-11-17 18:14:31 +0100

    feat: wip at maps

---
+++ maps/dictionary.go
@@ -0,0 +1,15 @@
+package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (dict Dictionary) Search(key string) (string, error) {
+	definition, ok := dict[key]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
