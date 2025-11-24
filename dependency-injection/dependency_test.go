6cc18f9c75fa7daf6097accc22b5bb625d9c90c0
npikall <nikolas.pikall@gmail.com>
2025-11-24 19:48:13 +0100

    feat: finished chapter dependency-injection

---
+++ dependency-injection/dependency_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "John")
+
+	got := buffer.String()
+	want := "Hello, John"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
