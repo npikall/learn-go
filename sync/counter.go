139e44782a0c643dd029aa0a3d0b6a2417706fce
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-27 17:58:53 +0100

    feat(sync): add mutual exclusion lock to counter

--- sync/counter.go
+++ sync/counter.go
@@ -1,10 +1,16 @@
 package counter
 
+import "sync"
+
 type Counter struct {
+	// Mutual Exclusion Lock
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
