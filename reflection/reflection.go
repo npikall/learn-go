2dd18ce5e08bb96072cac34e67655e76b622834d
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-27 16:33:24 +0100

    feat: added reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,29 @@
+package reflection
+
+import "reflect"
+
+func walk(x any, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := range val.NumField() {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := range val.Len() {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x any) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
+}
---
+++ reflection/reflection_test.go
@@ -0,0 +1,84 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         any
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"John"},
+			[]string{"John"},
+		},
+		{
+			"struct with two fields",
+			struct {
+				Name string
+				City string
+			}{"John", "London"},
+			[]string{"John", "London"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %q want %q", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
