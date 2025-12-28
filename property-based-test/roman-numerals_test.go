a14369ce9d667f9d7c4bcf2cc6ed344cfed7db8b
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-28 18:36:18 +0100

    refactor(property): use table driven tests

--- property-based-test/roman-numerals_test.go
+++ property-based-test/roman-numerals_test.go
@@ -3,20 +3,21 @@ package roman
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		name string
+		got  int
+		want string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ConvertToRoman(tt.got)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
+			if got != tt.want {
+				t.Errorf("got %q want %q", got, tt.want)
+			}
+		})
+	}
 }
