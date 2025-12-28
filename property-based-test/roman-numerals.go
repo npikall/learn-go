7aea92d0fbd8186309fd0271a9f4fecd770ab39e
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-28 18:50:47 +0100

    refactor(property): move 4 into string builder

--- property-based-test/roman-numerals.go
+++ property-based-test/roman-numerals.go
@@ -3,12 +3,12 @@ package roman
 import "strings"
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
 	var result strings.Builder
-
-	for range arabic {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
