d0c47fb5e8f36ae79e564fafb94e58f78fc10a74
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-28 18:45:46 +0100

    test(property): add case 4 -> IV

--- property-based-test/roman-numerals_test.go
+++ property-based-test/roman-numerals_test.go
@@ -11,6 +11,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (not more than 3 repeats)", 4, "IV"},
 	}
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
