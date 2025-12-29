df8221cd2fcffc1f9c1d5c2558bbaefe9b51d7d6
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-29 12:17:21 +0100

    feat(math): minimal code to pass test

---
+++ maths/clockface.go
@@ -0,0 +1,17 @@
+package clockface
+
+import (
+	"time"
+)
+
+// A Point represents a 2D Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
